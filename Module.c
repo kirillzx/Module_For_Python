783c416ab2596782843471efd899e801c50234d1
Kirill Zakharov <kirill.zakharov00@mail.ru>
2020-04-13 19:57:06 +0300

    Fix matrix

--- Module.c
+++ Module.c
@@ -3,11 +3,7 @@
 #include <math.h>
 
 
-double a[3][5]={
-    {4.4, 2.3, 3.2, 2.5, -5.0},
-    {4.1, 2.3, 3.3, 6.5, -2.3},
-    {4.4, 5.1, 3.2, 4.2, -1.2}
-};
+
 
 double b[4][4]={
     {4.2, 2.3, 3.2, 6.6},
@@ -65,48 +61,37 @@ double NormInfifnity(void *f, int n){
   return k;
 }
 
-// double matrixNormOne(void *f, int n, int l)
-// {
-//     double *a;
-//     double sum, k;
-//     int i, j, m;
-//     double new[5] = {0};
-//     a = f;
-//
-//     for(j=0; j<l; j++){
-//         sum = 0;
-//         for(i=0; i<n; i++){
-//             sum += fabs(a[i][j]);
-//         }
-//         new[j] = sum;
-//     }
-//     k = new[0];
-//     for(m=0; m<l; m++){
-//       if(new[m] > k){
-//         k = new[m];
-//       }
-//     }
-//
-//     return k;
-// }
+double matrixNormOne(double f[][5], int n, int l){
+    double sum, k;
+    int i, j, m;
+    double new[5] = {0};
 
-double matrixNormInfinity(void *f, int n, int l)
-{
-    double *a;
+    for(j=0; j<l; j++){
+        sum = 0;
+        for(i=0; i<n; i++){
+            sum += fabs(f[i][j]);
+        }
+        new[j] = sum;
+    }
+    k = new[0];
+    for(m=0; m<l; m++){
+      if(new[m] > k){
+        k = new[m];
+      }
+    }
+
+    return k;
+}
+
+double matrixNormInfinity(double f[][5], int n, int l){
     double sum, k;
     int i, j, m;
     double new[3] = {0};
-    a = f;
 
-    double c[3][5]={
-        {4.4, 2.3, 3.2, 2.5, -5.0},
-        {4.1, 2.3, 3.3, 6.5, -2.3},
-        {4.4, 5.1, 3.2, 4.2, -1.2}
-    };
     for(i=0; j<n; i++){
         sum = 0;
         for(j=0; j<l; j++){
-            sum += fabs(c[i][j]);
+            sum += fabs(f[i][j]);
         }
         new[i] = sum;
     }
@@ -125,13 +110,17 @@ int main()
     int n = 3;
     int p = 4;
     double x1[3] = {1, 2, -2};
+    double a[][5]={
+        {4.4, 2.3, 3.2, 2.5, -5.0},
+        {4.1, 2.3, 3.3, 6.5, -2.3},
+        {4.4, 5.1, 3.2, 4.2, -1.2}
+    };
+    printf("Euclidean norm: %3.3lf\n", NormEuclidean(x1, n));
+    printf("Manhattan distance: %3.3lf\n", NormManhattan(x1, n));
+    printf("Infinity norm: %3.3lf\n", NormInfifnity(x1, n));
+    printf("The Norm of the space Lp: %3.3lf\n", NormLp(x1, n, p));
 
-    // printf("Euclidean norm: %3.3lf\n", NormEuclidean(x1, n));
-    // printf("Manhattan distance: %3.3lf\n", NormManhattan(x1, n));
-    // printf("Infinity norm: %3.3lf\n", NormInfifnity(x1, n));
-    // printf("The Norm of the space Lp: %3.3lf\n", NormLp(x1, n, p));
-
-    // printf("%f\n", matrixNormOne(a, 3, 5));
-    printf("%f\n", matrixNormInfinity(a, 3, 5));
+    printf("The matrix norm one: %3.3lf\n", matrixNormOne(a, 3, 5));
+    printf("The matrix norm infinity: %3.3lf\n", matrixNormInfinity(a, 3, 5));
     return 0;
 }
