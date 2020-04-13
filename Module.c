61ddc7acf05bf40f790ad3cad51378eff0a472bd
Kirill Zakharov <kirill.zakharov00@mail.ru>
2020-04-13 15:23:40 +0300

    V1

--- Module.c
+++ Module.c
@@ -90,30 +90,35 @@ double NormInfifnity(void *f, int n){
 //     return k;
 // }
 
-// double matrixNormInfinity(void *f, int n, int l)
-// {
-//     double *a;
-//     double sum, k;
-//     int i, j, m;
-//     double new[3] = {0};
-//     a = f;
-//
-//     for(i=0; j<n; i++){
-//         sum = 0;
-//         for(j=0; j<l; j++){
-//             sum += fabs(a[i][j]);
-//         }
-//         new[i] = sum;
-//     }
-//     k = new[0];
-//     for(m=0; m<n; m++){
-//       if(new[m] > k){
-//         k = new[m];
-//       }
-//     }
-//
-//     return k;
-// }
+double matrixNormInfinity(void *f, int n, int l)
+{
+    double *a;
+    double sum, k;
+    int i, j, m;
+    double new[3] = {0};
+    a = f;
+
+    double c[3][5]={
+        {4.4, 2.3, 3.2, 2.5, -5.0},
+        {4.1, 2.3, 3.3, 6.5, -2.3},
+        {4.4, 5.1, 3.2, 4.2, -1.2}
+    };
+    for(i=0; j<n; i++){
+        sum = 0;
+        for(j=0; j<l; j++){
+            sum += fabs(c[i][j]);
+        }
+        new[i] = sum;
+    }
+    k = new[0];
+    for(m=0; m<n; m++){
+      if(new[m] > k){
+        k = new[m];
+      }
+    }
+
+    return k;
+}
 
 int main()
 {
@@ -121,12 +126,12 @@ int main()
     int p = 4;
     double x1[3] = {1, 2, -2};
 
-    printf("Euclidean norm: %3.3lf\n", NormEuclidean(x1, n));
-    printf("Manhattan distance: %3.3lf\n", NormManhattan(x1, n));
-    printf("Infinity norm: %3.3lf\n", NormInfifnity(x1, n));
-    printf("The Norm of the space Lp: %3.3lf\n", NormLp(x1, n, p));
+    // printf("Euclidean norm: %3.3lf\n", NormEuclidean(x1, n));
+    // printf("Manhattan distance: %3.3lf\n", NormManhattan(x1, n));
+    // printf("Infinity norm: %3.3lf\n", NormInfifnity(x1, n));
+    // printf("The Norm of the space Lp: %3.3lf\n", NormLp(x1, n, p));
 
     // printf("%f\n", matrixNormOne(a, 3, 5));
-    // printf("%f\n", matrixNormInfinity(a, 3, 5));
+    printf("%f\n", matrixNormInfinity(a, 3, 5));
     return 0;
 }
---
+++ sumMatrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define M 10
+#define N 5
+main() {
+    int a[N][M];
+    int sc[M];
+    int s, i, j;
+    srand(time(NULL));
+    for (i=0; i<M; i++) sc[i] = 0;
+    for (i=0; i<N; i++) {
+        s = 0;
+        for (j=0; j<M; j++) {
+            a[i][j] = rand() % 10;
+            printf("%5d", a[i][j]);
+            s += a[i][j];
+            sc[j] += a[i][j];
+        }
+        printf("   |%d\n", s);
+    }
+    for (i=0; i<M; i++)
+        printf("%5s", "--");
+    printf("\n");
+    for (i=0; i<M; i++)
+        printf("%5d", sc[i]);
+    printf("\n");
+}
